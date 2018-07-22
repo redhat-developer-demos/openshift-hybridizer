7c864dc94632d611c8c086e53c1cf22ebe668d3b
Kamesh Sampath <kamesh.sampath@hotmail.com>
2018-07-23 00:03:54 +0530

    GCP Provisioner now - uses or generates SSH to be used by cloud_user - the SSH keys provided will be added to the user-metadata - Doc update for the key files name/path

--- deprovision.sh
+++ deprovision.sh
@@ -5,14 +5,14 @@ set -e
 _CURR_DIR="$( cd "$(dirname "$0")" ; pwd -P )"
 
 # Delete all the existing Cloud Providers Hosts folder
-rm -rf inventory/gcp
-rm -rf inventory/aws
-rm -rf inventory/azr
+rm -rf out
 
 docker run -t -v ${_CURR_DIR}/artifacts:/runner/artifacts \
+ -v ${_CURR_DIR}/out:/runner/out \
  -v ${_CURR_DIR}/env:/runner/env \
  -v ${_CURR_DIR}/inventory:/runner/inventory \
  -v ${_CURR_DIR}/project:/runner/project \
  -v ${_CURR_DIR}/roles:/runner/roles \
+ -e PROJECT_DIR=${_CURR_DIR}/project \
  -e RUNNER_PLAYBOOK=deprovision-cloud-resources.yaml \
  kameshsampath/ansible-runner:latest
--- provision.sh
+++ provision.sh
@@ -5,9 +5,11 @@ set -e
 _CURR_DIR="$( cd "$(dirname "$0")" ; pwd -P )"
 
 docker run -t -v ${_CURR_DIR}/artifacts:/runner/artifacts \
+ -v ${_CURR_DIR}/out:/runner/out \
  -v ${_CURR_DIR}/env:/runner/env \
  -v ${_CURR_DIR}/inventory:/runner/inventory \
  -v ${_CURR_DIR}/project:/runner/project \
  -v ${_CURR_DIR}/roles:/runner/roles \
+ -e PROJECT_DIR=${_CURR_DIR}/project \
  -e RUNNER_PLAYBOOK=provision-cloud-resources.yaml \
  docker.io/kameshsampath/ansible-runner:latest
