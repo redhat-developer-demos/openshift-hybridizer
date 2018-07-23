89373ee30816855ad9cdd047a45d674cfc02fe44
Kamesh Sampath <kamesh.sampath@hotmail.com>
2018-07-23 17:41:56 +0530

    - Handling Azure network reconfig via host_prepare.yaml - Fixed azure.conf not generated - SELinux label fixes
    WIP
    - docker image rebuild with proper user permissions

--- deprovision.sh
+++ deprovision.sh
@@ -4,12 +4,12 @@ set -e
 
 _CURR_DIR="$( cd "$(dirname "$0")" ; pwd -P )"
 
-docker run -t -u `id -u` -v ${_CURR_DIR}/artifacts:/runner/artifacts:Z \
- -v ${_CURR_DIR}/out:/runner/out:Z \
- -v ${_CURR_DIR}/env:/runner/env:Z \
- -v ${_CURR_DIR}/inventory:/runner/inventory:Z \
- -v ${_CURR_DIR}/project:/runner/project:Z \
- -v ${_CURR_DIR}/roles:/runner/roles:Z \
+docker run -t -u `id -u` -v ${_CURR_DIR}/artifacts:/runner/artifacts:z \
+ -v ${_CURR_DIR}/out:/runner/out:z \
+ -v ${_CURR_DIR}/env:/runner/env:z \
+ -v ${_CURR_DIR}/inventory:/runner/inventory:z \
+ -v ${_CURR_DIR}/project:/runner/project:z \
+ -v ${_CURR_DIR}/roles:/runner/roles:z \
  -e PROJECT_DIR=${_CURR_DIR}/project \
  -e RUNNER_PLAYBOOK=deprovision-cloud-resources.yaml \
  kameshsampath/ansible-runner:latest
