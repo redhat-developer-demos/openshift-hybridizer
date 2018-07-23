344ef816b16081f0443d8a8164816be60227d6f6
Kamesh Sampath <kamesh.sampath@hotmail.com>
2018-07-23 22:45:51 +0530

    - Document updates - variables update with docs and ordering - SELinux Labels

--- provision.sh
+++ provision.sh
@@ -4,12 +4,12 @@ set -e
 
 _CURR_DIR="$( cd "$(dirname "$0")" ; pwd -P )"
 
-docker run -t -u `id -u` -v ${_CURR_DIR}/artifacts:/runner/artifacts:z,rw \
- -v ${_CURR_DIR}/out:/runner/out:z,rw \
- -v ${_CURR_DIR}/env:/runner/env:z,ro \
- -v ${_CURR_DIR}/inventory:/runner/inventory:z,rw \
- -v ${_CURR_DIR}/project:/runner/project:z,rw \
- -v ${_CURR_DIR}/roles:/runner/roles:z,ro \
+docker run -t -u `id -u` -v ${_CURR_DIR}/artifacts:/runner/artifacts:Z \
+ -v ${_CURR_DIR}/out:/runner/out:Z \
+ -v ${_CURR_DIR}/env:/runner/env:Z \
+ -v ${_CURR_DIR}/inventory:/runner/inventory:Z \
+ -v ${_CURR_DIR}/project:/runner/project:Z \
+ -v ${_CURR_DIR}/roles:/runner/roles:Z \
  -e PROJECT_DIR=${_CURR_DIR}/project \
  -e RUNNER_PLAYBOOK=provision-cloud-resources.yaml \
  docker.io/kameshsampath/ansible-runner:latest
