e02c29989c7506157496e7b058dce2ba43cca877
Kamesh Sampath <kamesh.sampath@hotmail.com>
2018-07-23 10:30:02 +0530

    - Added SELinux labels to volume mounts - Added Azure SSH KeyPair generation

--- deprovision.sh
+++ deprovision.sh
@@ -4,15 +4,15 @@ set -e
 
 _CURR_DIR="$( cd "$(dirname "$0")" ; pwd -P )"
 
-# Delete all the existing Cloud Providers Hosts folder
-rm -rf out
-
-docker run -t -v ${_CURR_DIR}/artifacts:/runner/artifacts \
- -v ${_CURR_DIR}/out:/runner/out \
- -v ${_CURR_DIR}/env:/runner/env \
- -v ${_CURR_DIR}/inventory:/runner/inventory \
- -v ${_CURR_DIR}/project:/runner/project \
- -v ${_CURR_DIR}/roles:/runner/roles \
+docker run -t -u `id -u` -v ${_CURR_DIR}/artifacts:/runner/artifacts:Z \
+ -v ${_CURR_DIR}/out:/runner/out:Z \
+ -v ${_CURR_DIR}/env:/runner/env:Z \
+ -v ${_CURR_DIR}/inventory:/runner/inventory:Z \
+ -v ${_CURR_DIR}/project:/runner/project:Z \
+ -v ${_CURR_DIR}/roles:/runner/roles:Z \
  -e PROJECT_DIR=${_CURR_DIR}/project \
  -e RUNNER_PLAYBOOK=deprovision-cloud-resources.yaml \
  kameshsampath/ansible-runner:latest
+
+# Delete all the existing Cloud Providers Hosts folder
+rm -rf out
--- provision.sh
+++ provision.sh
@@ -4,12 +4,12 @@ set -e
 
 _CURR_DIR="$( cd "$(dirname "$0")" ; pwd -P )"
 
-docker run -t -v ${_CURR_DIR}/artifacts:/runner/artifacts \
- -v ${_CURR_DIR}/out:/runner/out \
- -v ${_CURR_DIR}/env:/runner/env \
- -v ${_CURR_DIR}/inventory:/runner/inventory \
- -v ${_CURR_DIR}/project:/runner/project \
- -v ${_CURR_DIR}/roles:/runner/roles \
+docker run -t -u `id -u` -v ${_CURR_DIR}/artifacts:/runner/artifacts:Z \
+ -v ${_CURR_DIR}/out:/runner/out:Z \
+ -v ${_CURR_DIR}/env:/runner/env:Z \
+ -v ${_CURR_DIR}/inventory:/runner/inventory:Z \
+ -v ${_CURR_DIR}/project:/runner/project:Z \
+ -v ${_CURR_DIR}/roles:/runner/roles:Z \
  -e PROJECT_DIR=${_CURR_DIR}/project \
  -e RUNNER_PLAYBOOK=provision-cloud-resources.yaml \
  docker.io/kameshsampath/ansible-runner:latest
